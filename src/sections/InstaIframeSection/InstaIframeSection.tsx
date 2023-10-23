246511a9bb064a27b008b68480f972244f47d201
Daniil <zyabin@yandex.ru>
2023-10-23 20:57:52 +0300

    feat: add instagram iframe

--- src/sections/InstaIframeSection/InstaIframeSection.tsx
+++ src/sections/InstaIframeSection/InstaIframeSection.tsx
@@ -1,9 +1,13 @@
 import styles from './InstaIframeSection.module.scss';
+import {InstagramEmbed} from "react-social-media-embed";
 
 export function InstaIframeSection() {
     return (
         <section className={styles.root}>
-            
+            <InstagramEmbed
+                url="https://www.instagram.com/reel/Cl6D_oQOFot/?utm_source=ig_embed&amp;utm_campaign=loading"
+                retryDisabled={true}
+            />
         </section>
     );
 }
